34f278b9c9a31e909ebc61b359dece9552c3ebf4
pixel-glitch <keoaniket@gmail.com>
2023-01-05 14:49:35 +0600

    redux

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 function connectDB(){
 
     try{
-        mongoose.connect('mongodb+srv://tanvir:tanvir@cluster0.hy5ioh7.mongodb.net/car-rent' , {useUnifiedTopology: true,useNewUrlParser: true});
+        mongoose.connect('mongodb+srv://tanvir:tanvir@user.8hirx51.mongodb.net/car-rent' , {useUnifiedTopology: true,useNewUrlParser: true});
         console.log("MongoDB Connected");
     }
     catch(err){
@@ -11,9 +11,9 @@ function connectDB(){
     }
     // const connection = mongoose.connect
 
-    // connection.on('connected', () => {
+    // connection.on('connected', () => {  mongodb+srv://tanvir:tanvir@cluster0.hy5ioh7.mongodb.net/car-rent
     //     console.log('Mongo DB connected ok')
-    // })
+    // }) mongodb+srv://tanvir:<password>@user.8hirx51.mongodb.net/test
 
     // connection.on('error', (err) => {
     //         console.log('Can not connect to MongoDB')
